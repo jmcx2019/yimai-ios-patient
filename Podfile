96e45ac072d890ebf2b5651c637771f1adc06f5a
why <why>
2016-08-13 10:46:31 +0800

    init patient

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'YiMaiPatient' do
+  pod 'AFNetworking'
+  pod 'SwiftyJSON'
+  pod 'Neon'
+  pod 'swiftScan'
+  pod 'Proposer'
+  pod 'SwiftDate'
+  pod 'Graph'
+end
+
---
+++ YiMaiPatient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YiMaiPatient
+//
+//  Created by ios-dev on 16/8/13.
+//  Copyright © 2016年 yimai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
